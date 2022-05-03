5183837c300d552bfec35a1f10d1f8589f5fb663
Kimmo Linna <kimmo.linna@gmail.com>
2022-05-03 18:11:09 +0300

    kerneldir

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 BASEDIR=$(dirname "$0")
 PYVER="$(python3 --version | sed 's/.*\(3\.[0-9]*\).*/\1/')"
 
-KERNELDIR="/usr/local/share/jupyter/kernels"
+KERNELDIR="$HOME/.local/share/jupyter/kernels"
 SITEDIR="$(python3 -m site --user-site)"
 
 mkdir -p "$KERNELDIR"
